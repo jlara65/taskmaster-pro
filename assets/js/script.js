6c231019f952c9d8712c51ed77c10b6011548192
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-08-12 21:21:35 -0500

    UX enchacements

--- assets/js/script.js
+++ assets/js/script.js
@@ -146,7 +146,7 @@ $(".list-group").on("change", "input[type='text']", function() {
   // Pass task's <li> element into auditTask() to check new due date
   auditTask($(taskSpan)
   .closest(".list-group-item"));
-});
+}); 
 
 
 // modal was triggered
@@ -162,7 +162,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -199,15 +199,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log(ui);
   },
   deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     console.log(ui);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
     console.log(event);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
     console.log(event);
   },
   update: function() {
@@ -248,11 +254,14 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log(ui);
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("out");
   }
 });
@@ -286,3 +295,11 @@ var auditTask = function(taskEl) {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function() {
+    auditTask($(this));
+  });
+}, (1000*60)*30);
+
+
